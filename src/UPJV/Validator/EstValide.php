97a175a773db4f86fe92f1a29af7ac1cf2516cdf
Axel Thellier <ax.thellier@gmail.com>
2022-03-23 16:40:16 +0100

    ajout de documentation à EstValide

--- src/UPJV/Validator/EstValide.php
+++ src/UPJV/Validator/EstValide.php
@@ -1,14 +1,27 @@
 <?php
 
+/**
+ * Vérifie que
+ */
+
 namespace UPJV\Validator;
 
+/**
+ * Class EstValide implémente les méthode build & check
+ */
 class EstValide implements ValidatorInterface
 {
+    /**
+     * ceci est de la documentation pour la fonction build
+     */
     public function build(array $param): object
     {
         return $this;
     }
 
+    /**
+     * ceci est de la documentation pour la fonction check
+     */
     public function check($input): bool
     {
         return true;
