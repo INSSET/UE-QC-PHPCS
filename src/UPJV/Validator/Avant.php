9f236c2c1b97303d57bfc28ee615f1504e6ee7dc
= <=>
2022-03-29 14:39:10 +0200

    creation page Avant.php

---
+++ src/UPJV/Validator/Avant.php
@@ -0,0 +1,32 @@
+ * Class Avant implémente les méthode build & check
+ */
+class Avant implements ValidatorInterface
+{
+    protected int $size;
+
+    /**
+     * Récupère la taille minimum spécifier dans la conf
+     *
+     * @param array $param
+     *
+     * @return object
+     */
+    public function build(array $param): object
+    {
+        $this->size = $param[0];
+
+        return $this;
+    }
+
+    /**
+     * Test la longueur de la chaine de caractères
+     *
+     * @param $input
+     *
+     * @return bool
+     */
+    public function check($input): bool
+    {
+        return (strlen($input) >= $this->size);
+    }
+}
