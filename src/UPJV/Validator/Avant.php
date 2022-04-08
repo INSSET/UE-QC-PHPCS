984c09cf90da8f14c277bf44d21afe0412b4d6a1
= <=>
2022-04-08 14:20:53 +0200

    Essai 1

--- src/UPJV/Validator/Avant.php
+++ src/UPJV/Validator/Avant.php
@@ -36,8 +36,8 @@ class Avant implements ValidatorInterface
      */
     public function check($input): bool
     {
-	$d = explode('-', $input);
+	    $d = explode('-', $input);
 	
-	return (mktime(0, 0, 0, $d[1], $d[0], $d[2]) < $this->size);
+	    return (mktime(0, 0, 0, $d[1], $d[0], $d[2]) < $this->size);
     }
 }
