759cc1b2436351b273d883537a88f0791e3772cd
ClementDINSSET <clementdubois73@gmail.com>
2022-04-26 16:23:32 +0200

    Test EgaleA Clement

---
+++ test/units/EgaleATest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace PHPUnit\Framework\TestCase;
+use UPJV\Validator\Min;
+
+require_once __DIR__ . '/../../src/UPJV/Validator/ValidatorEngine.php';
+require_once __DIR__ . '/../../src/UPJV/Validator/ValidatorInterface.php';
+require_once __DIR__ . '/../../src/UPJV/Validator/Min.php';
+
+/**
+ * @covers \UPJV\Validator\Min
+ */
+class EgaleATest extends \PHPUnit\Framework\TestCase
+{
+    public function testBuild(): object
+    {
+        $validateur = new Min();
+        $obj = $validateur->build( [7]);
+        $this->assertInstanceOf(\UPJV\Validator\ValidatorInterface::class , $obj);
+        return $obj;
+    }
+
+    /**
+     * @depends testBuild
+     */
+    public function testCheckValide( object $obj ): void
+    {
+        $this->assertEquals(true, $obj->check(7));
+    }
+
+    /**
+     * @depends testBuild
+     */
+    public function testCheckNotValide( object $obj ): void
+    {
+        $this->assertEquals(false, $obj->check(6));
+    }
+
+}
