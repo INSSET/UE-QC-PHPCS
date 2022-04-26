326808b29ff9eec3765a6248c03ea79965f90f3b
LaetitiaMagniez <lmagniez03@gmail.com>
2022-04-26 17:07:43 +0200

    créations du fichier EstUneImageTest.php

---
+++ test/units/EstUneImageTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace PHPUnit\Framework\TestCase;
+use UPJV\Validator\EstUneImage;
+
+require_once __DIR__ . '/../../src/UPJV/Validator/ValidatorEngine.php';
+require_once __DIR__ . '/../../src/UPJV/Validator/ValidatorInterface.php';
+require_once __DIR__ . '/../../src/UPJV/Validator/EstUneImage.php';
+
+/**
+ * @covers \UPJV\Validator\Min
+ */
+class EstUneImageTest extends \PHPUnit\Framework\TestCase
+{
+    public function testBuild(): object
+    {
+        $validateur = new EstUneImage();
+        $obj = $validateur->build( [2]);
+        $this->assertInstanceOf(\UPJV\Validator\ValidatorInterface::class , $obj);
+        return $obj;
+    }
+
+    /**
+     * @depends testBuild
+     */
+    public function testCheckValide( object $obj ): void
+    {
+        $this->assertEquals(true, $obj->check(3));
+    }
+
+    /**
+     * @depends testBuild
+     */
+    public function testCheckNotValide( object $obj ): void
+    {
+        $this->assertEquals(false, $obj->check(1));
+    }
+
+}
