94a81be6fcef641a200701b09ea1a97b3e23478c
Clément Dubois <99265134+ClementDINSSET@users.noreply.github.com>
2022-04-27 10:16:29 +0200

    Test EgaleA Clément (#25)
    * Test EgaleA Clement
    
    * Test EgaleA Clement 2
    
    * Test EgaleA
    
    * Test EgaleA final

---
+++ test/units/EgaleATest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace PHPUnit\Framework\TestCase;
+use UPJV\Validator\EgaleA;
+
+require_once __DIR__ . '/../../src/UPJV/Validator/ValidatorEngine.php';
+require_once __DIR__ . '/../../src/UPJV/Validator/ValidatorInterface.php';
+require_once __DIR__ . '/../../src/UPJV/Validator/EgaleA.php';
+
+/**
+ * @covers \UPJV\Validator\EgaleA
+ */
+class EgaleATest extends \PHPUnit\Framework\TestCase
+{
+    public function testBuild(): object
+    {
+        $validateur = new EgaleA();
+        $obj = $validateur->build( [7]);
+        $this->assertInstanceOf(\UPJV\Validator\ValidatorInterface::class , $obj);
+        return $obj;
+    }
+
+    /**
+     * @depends testBuild
+     */
+    public function testCheckValide( object $obj ): void
+    {
+        $this->assertEquals(true, $obj->check("Clement"));
+    }
+
+    /**
+     * @depends testBuild
+     */
+    public function testCheckNotValide( object $obj ): void
+    {
+        $this->assertEquals(false, $obj->check("Tintin"));
+    }
+
+}
